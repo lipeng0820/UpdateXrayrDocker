46c195f5164cfe00f5e929df186c672efb5bf09e
lipeng0820 <lipeng0820@qq.com>
2023-09-05 02:08:55 +0800

    Update updatexrayrdocker.sh

--- updatexrayrdocker.sh
+++ updatexrayrdocker.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
-# 切换到用户的主目录
+# 0. 切换到用户主目录
 cd ~
 
-# 获取所有 xrayr 的镜像ID
-xrayr_images=$(docker images | grep "ghcr.io/xrayr-project/xrayr" | awk '{print $3}')
+# 1. 停止并删除正在运行的 xrayr 容器
+docker stop xrayr
+docker rm xrayr
 
-# 删除这些镜像
-for image in $xrayr_images
-do
-    docker rmi $image
-done
+# 2. 删除旧的 xrayr 镜像
+docker rmi $(docker images ghcr.io/xrayr-project/xrayr -q)
 
-# 拉取最新的 xrayr master 版本
+# 3. 拉取最新的 xrayr 镜像
 docker pull ghcr.io/xrayr-project/xrayr:master
 
-# 获取所有正在运行的 xrayr 容器的 ID
-containers=$(docker ps | grep "ghcr.io/xrayr-project/xrayr:master" | awk '{print $1}')
+# 4. 运行新的 xrayr 容器
+docker run --restart=always --name xrayr -d -v ~/config.yml:/etc/XrayR/config.yml --network=host ghcr.io/xrayr-project/xrayr:master
 
-# 遍历每一个容器
-for container in $containers
-do
-    echo "正在停止容器: $container"
-    docker stop $container
-
-    echo "正在删除容器: $container"
-    docker rm $container
-done
-
-# 启动新的 xrayr 容器
-docker run -d --name xrayr ghcr.io/xrayr-project/xrayr:master
-
-echo "所有 xrayr 容器已更新到最新版本"
-
-# 显示所有容器的状态
+# 5. 打印当前运行的容器状态
 docker ps -a
